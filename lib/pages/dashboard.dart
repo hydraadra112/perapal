56a26bd891375c7eaa02b853b6cf0ab8ce66e63e
Els Dave Constantino <elsdave.constantino@wvsu.edu.ph>
2024-05-05 08:48:57 +0800

    added total fund indicator on dashboard

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+import 'package:perapal/utils/style.dart';
 
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
-  @override
+@override
   Widget build(BuildContext context) {
-    return const Scaffold();
-    
+    return Scaffold(
+      body: Padding(
+        padding: EdgeInsets.all(medium),
+        child: SizedBox(
+          height: large,
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  "Your Balance:",
+                  style: heading3,
+                ),
+                SizedBox(height: 5), // Adjust the spacing between text and button
+                Container(
+                  decoration: BoxDecoration(
+                    color: blue,
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(15),
+                      bottom: Radius.circular(15),
+                    ),
+                  ),
+                  width: MediaQuery.of(context).size.width / 2,
+                  child: Center(
+                    child: Container(
+                      child: Text(
+                        "₱ 50 000",
+                        style: heading2L,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
